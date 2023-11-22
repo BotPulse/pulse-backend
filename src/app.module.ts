fd7cb3e85f5b019da06527b9b3a43f178637c2e5
gersonmayer <g.mayerduran@gmail.com>
2023-11-22 14:26:13 -0500

    feat: added and injected config module to httpService Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShareModuleModule } from './share-module/share-module.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ShareModuleModule],
+  imports: [
+    ShareModuleModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

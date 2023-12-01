309fbc4776c9ac52642c611c13ed0680ebc03911
gersonmayer <g.mayerduran@gmail.com>
2023-12-01 10:54:58 -0500

    Refactor: created StrategyMap custom provider

--- src/incoming/incoming.service.ts
+++ src/incoming/incoming.service.ts
@@ -1,40 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { WebhookPayload } from './dto/webhook-payload';
-import { OutcomingService } from 'src/outcoming/outcoming.service';
 import { IncomingStrategyService } from './strategies/incoming-strategy.service';
-import { UnknownPayloadStrategy } from './strategies/unknown-message.service';
-import { TextMessageStrategy } from './strategies/text-message.strategy';
-import { MessageStatusStrategy } from './strategies/message-status.strategy';
 import {
   IncomingWhatsappRequestStrategy,
   IncomingWhatsappRequestStrategyType,
 } from './strategies/strategy-interfaces';
 import { ValueKeys } from './dto/webhook-payload';
+
 @Injectable()
 export class IncomingService {
   constructor(
+    @Inject('StrategiesMap')
+    private strategiesMap: Map<
+      IncomingWhatsappRequestStrategyType,
+      IncomingWhatsappRequestStrategy
+    >,
     private incomingStrategyService: IncomingStrategyService,
-    private outcomingService: OutcomingService,
-    private textMessageStrategy: TextMessageStrategy,
-    private unknownPayloadStrategy: UnknownPayloadStrategy,
-    private messageStatusStrategy: MessageStatusStrategy,
   ) {}
 
   private getStrategy(
     requestBody: WebhookPayload,
   ): IncomingWhatsappRequestStrategy {
     const value = requestBody.entry[0].changes[0].value;
-    const strategies = new Map<
-      IncomingWhatsappRequestStrategyType,
-      IncomingWhatsappRequestStrategy
-    >([
-      [IncomingWhatsappRequestStrategyType.TEXT, this.textMessageStrategy],
-      [IncomingWhatsappRequestStrategyType.STATUS, this.messageStatusStrategy],
-      [
-        IncomingWhatsappRequestStrategyType.UNKNOWN,
-        this.unknownPayloadStrategy,
-      ],
-    ]);
+    const strategies = this.strategiesMap;
     if (value.hasOwnProperty(ValueKeys.MESSAGES)) {
       const type = value.messages[0].type;
       const strategy = strategies.get(
--- src/incoming/strategies/text-message.strategy.ts
+++ src/incoming/strategies/text-message.strategy.ts
@@ -1,9 +1,10 @@
 import { WebhookPayload } from '../dto/webhook-payload';
 import { IncomingWhatsappRequestStrategy } from './strategy-interfaces';
-import { Injectable } from '@nestjs/common';
+import { Injectable, Provider } from '@nestjs/common';
 import { CustomWhatsappAnswer } from 'src/outcoming/dto/custom-response.dto';
 import { OutcomingService } from 'src/outcoming/outcoming.service';
 import { OpenAIChat } from '../chains/main-chain';
+
 @Injectable()
 export class TextMessageStrategy implements IncomingWhatsappRequestStrategy {
   constructor(
@@ -33,3 +34,8 @@ export class TextMessageStrategy implements IncomingWhatsappRequestStrategy {
     return response;
   }
 }
+
+export const TextMessageStrategyProvider: Provider = {
+  provide: TextMessageStrategy,
+  useClass: TextMessageStrategy,
+};

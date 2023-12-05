2a2c0dd03920c9d509eb22aac6f3f042e722558c
Gerson <g.mayerduran@gmail.com>
2023-12-05 13:20:40 -0500

    Feat: todo added

--- src/incoming/chains/main-chain.ts
+++ src/incoming/chains/main-chain.ts
@@ -14,6 +14,7 @@ import { ConversationChain } from 'langchain/chains';
 export class OpenAIChat {
   constructor(private readonly configService: ConfigService) {}
 
+  //TODO implement a Queue data structure that also eliminates buffers that are < 24h
   public async getAnswer(input: string): Promise<string> {
     const openAIApiKey = this.configService.get<string>('OPENAI_API_KEY');
     const model = new ChatOpenAI({
@@ -33,6 +34,9 @@ export class OpenAIChat {
       prompt: chatPromptTemplate,
       llm: model,
     });
+    //const messages = chain.memory.getMessages();
+    const memoryVariables = await chain.memory.loadMemoryVariables({});
+    console.log(memoryVariables.history);
     const response = await chain.call({
       input,
     });

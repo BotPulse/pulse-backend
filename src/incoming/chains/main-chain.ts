00f4b8d358d7fa547f900d826bb25a6c95433050
Gerson <g.mayerduran@gmail.com>
2023-12-06 11:26:52 -0500

    feat: Added initial message condition, refactored instace of buffer

--- src/incoming/chains/main-chain.ts
+++ src/incoming/chains/main-chain.ts
@@ -12,16 +12,20 @@ import { ConversationChain } from 'langchain/chains';
 
 @Injectable()
 export class OpenAIChat {
+  //TODO implement a Queue data structure that also eliminates buffers that are 24h old
+  //TODO implement a better way to store initial messages
   private conversationBuffer = new Map<string, BufferMemory>();
+  private model = new ChatOpenAI({
+    temperature: 0,
+    openAIApiKey: this.configService.get<string>('OPENAI_API_KEY'),
+  });
+  private buffer = new BufferMemory({
+    returnMessages: true,
+    memoryKey: 'history',
+  });
   constructor(private readonly configService: ConfigService) {}
 
-  //TODO implement a Queue data structure that also eliminates buffers that are 24h old
   public async getAnswer(user: string, input: string): Promise<string> {
-    const openAIApiKey = this.configService.get<string>('OPENAI_API_KEY');
-    const model = new ChatOpenAI({
-      temperature: 0,
-      openAIApiKey,
-    });
     const systemPromptTemplate = `Transforma el siguiente {text},
     que contiene expresiones ofensivas, en una versión más respetuosa y cortés:`;
     const chatPromptTemplate = ChatPromptTemplate.fromMessages([
@@ -29,16 +33,19 @@ export class OpenAIChat {
       new MessagesPlaceholder('history'),
       HumanMessagePromptTemplate.fromTemplate(`{text}`),
     ]);
-    const buffer =
-      this.conversationBuffer.get(user) ||
-      new BufferMemory({ returnMessages: true, memoryKey: 'history' });
-
     const chain = new ConversationChain({
-      memory: buffer,
+      memory: this.buffer,
       prompt: chatPromptTemplate,
-      llm: model,
+      llm: this.model,
     });
-
+    const messages = await this.buffer.chatHistory.getMessages();
+    if (messages.length === 0) {
+      const greetings = `Hola mi nombre es juanito, y traduzco puteadas, 
+      por ejemplo si me dices: que eres bobo o tu mama te peina?
+      yo te respondere algo como: ¿Eres un poco despistado o tu madre te ayuda con el peinado?`;
+      await chain.memory.saveContext({ input }, { output: greetings });
+      return greetings;
+    }
     const response = await chain.call({
       text: input,
     });

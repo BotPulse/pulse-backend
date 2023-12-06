b4826a470227d0efe29e75ecc3aa0b62fb6c3fc4
Gerson <g.mayerduran@gmail.com>
2023-12-06 13:26:13 -0500

    Feat: added LimetedMap structure

--- src/incoming/chains/asistant-prompt.ts
+++ src/incoming/chains/asistant-prompt.ts
@@ -3,3 +3,10 @@ debes poder ofrecerle productos al cliente,
 los productos disponibles son: tablas, ruedas, rulimanes (o tambien llamadas bearings o baleros) y
 trucks (o tambien llamados ejes) , no debe responde otro tipo de preguntas que no sean 
 relacionadas con las patinetas y su venta.`;
+
+export const greetings = `Hola mi nombre es Juanito, y traduzco puteadas, 
+por ejemplo si me dices: que eres bobo o tu mama te peina?
+yo te respondere algo como: ¿Eres un poco despistado o tu madre te ayuda con el peinado?`;
+
+export const systemPromptTemplate = `Transforma el siguiente {text},
+que contiene expresiones ofensivas, en una versión más respetuosa y cortés:`;
--- src/incoming/chains/main-chain.ts
+++ src/incoming/chains/main-chain.ts
@@ -5,48 +5,35 @@ import {
   ChatPromptTemplate,
   HumanMessagePromptTemplate,
   SystemMessagePromptTemplate,
-  MessagesPlaceholder,
 } from 'langchain/prompts';
-import { BufferMemory } from 'langchain/memory';
 import { ConversationChain } from 'langchain/chains';
-
+import { systemPromptTemplate, greetings } from './asistant-prompt';
+import { LimitedSizeMap } from './limited-size-map';
 @Injectable()
 export class OpenAIChat {
   //TODO implement a Queue data structure that also eliminates buffers that are 24h old
-  //TODO implement a better way to store initial messages
-  private conversationBuffer = new Map<string, BufferMemory>();
+  private conversationIniciated = new LimitedSizeMap();
   private model = new ChatOpenAI({
     temperature: 0,
     openAIApiKey: this.configService.get<string>('OPENAI_API_KEY'),
   });
-  private buffer = new BufferMemory({
-    returnMessages: true,
-    memoryKey: 'history',
+  private chatPromptTemplate = ChatPromptTemplate.fromMessages([
+    SystemMessagePromptTemplate.fromTemplate(systemPromptTemplate),
+    HumanMessagePromptTemplate.fromTemplate(`{text}`),
+  ]);
+  private chain = new ConversationChain({
+    prompt: this.chatPromptTemplate,
+    llm: this.model,
   });
   constructor(private readonly configService: ConfigService) {}
 
   public async getAnswer(user: string, input: string): Promise<string> {
-    const systemPromptTemplate = `Transforma el siguiente {text},
-    que contiene expresiones ofensivas, en una versión más respetuosa y cortés:`;
-    const chatPromptTemplate = ChatPromptTemplate.fromMessages([
-      SystemMessagePromptTemplate.fromTemplate(systemPromptTemplate),
-      new MessagesPlaceholder('history'),
-      HumanMessagePromptTemplate.fromTemplate(`{text}`),
-    ]);
-    const chain = new ConversationChain({
-      memory: this.buffer,
-      prompt: chatPromptTemplate,
-      llm: this.model,
-    });
-    const messages = await this.buffer.chatHistory.getMessages();
-    if (messages.length === 0) {
-      const greetings = `Hola mi nombre es juanito, y traduzco puteadas, 
-      por ejemplo si me dices: que eres bobo o tu mama te peina?
-      yo te respondere algo como: ¿Eres un poco despistado o tu madre te ayuda con el peinado?`;
-      await chain.memory.saveContext({ input }, { output: greetings });
+    const hasConversationIniciated = this.conversationIniciated.get(user);
+    if (!hasConversationIniciated) {
+      this.conversationIniciated.set(user, true);
       return greetings;
     }
-    const response = await chain.call({
+    const response = await this.chain.call({
       text: input,
     });
     return response?.response;

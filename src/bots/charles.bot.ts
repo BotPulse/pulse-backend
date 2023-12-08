02935d2a08d47251e27ef19bdd45c4dfc2ee8dfd
Gerson Mayer <g.mayerduran@gmail.com>
2023-12-08 17:09:22 -0500

    Feat:adding agent

--- src/bots/charles.bot.ts
+++ src/bots/charles.bot.ts
@@ -1,47 +1,44 @@
 import { Injectable, Provider } from '@nestjs/common';
 import { BotInterface } from './bot-interface.bot';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { OpenAI } from 'langchain/llms/openai';
 import {
   ChatPromptTemplate,
-  HumanMessagePromptTemplate,
-  MessagesPlaceholder,
-  SystemMessagePromptTemplate,
+  // HumanMessagePromptTemplate,
+  // MessagesPlaceholder,
+  // SystemMessagePromptTemplate,
+  PromptTemplate,
 } from 'langchain/prompts';
 import { ConversationChain } from 'langchain/chains';
 import { LimitedSizeMap } from './data-structures/limited-size-map';
 import { ConfigService } from '@nestjs/config';
 import { charlesPromptTemplate } from './prompts/asistant-prompt';
 import { BufferMemory } from 'langchain/memory';
+import {
+  AgentExecutor,
+  initializeAgentExecutorWithOptions,
+} from 'langchain/agents';
+import { RunnableSequence } from 'langchain/schema/runnable';
+import { AgentStep, BaseMessage } from 'langchain/schema';
+import { formatLogToString } from 'langchain/agents/format_scratchpad/log';
+import { renderTextDescription } from 'langchain/tools/render';
+import { ReActSingleInputOutputParser } from 'langchain/agents/react/output_parser';
+import { DynamicTool } from 'langchain/tools';
 
 @Injectable()
 export class CharlesBot implements BotInterface {
   private userHistory = new LimitedSizeMap();
   private phoneNumber: string;
   private model: ChatOpenAI;
-  private chatPromptTemplate: ChatPromptTemplate;
-  private chain: ConversationChain;
   private memory: BufferMemory;
-  constructor(private configService: ConfigService) {
-    this.model = new ChatOpenAI({
-      temperature: 0,
-      openAIApiKey: this.configService.get<string>('OPENAI_API_KEY'),
-    });
-    this.chatPromptTemplate = ChatPromptTemplate.fromMessages([
-      SystemMessagePromptTemplate.fromTemplate(charlesPromptTemplate),
-      new MessagesPlaceholder('history'),
-      HumanMessagePromptTemplate.fromTemplate(`{text}`),
-      //HumanMessagePromptTemplate.fromTemplate(`{name}`),
-    ]);
-    this.memory = new BufferMemory({
-      returnMessages: true,
-      memoryKey: 'history',
-    });
-    this.chain = new ConversationChain({
-      prompt: this.chatPromptTemplate,
-      llm: this.model,
-      memory: this.memory,
-    });
-  }
+  private tools: DynamicTool[];
+  private prompt: PromptTemplate;
+  private toolNames: string[];
+  private promptWithInputs: any;
+  private runnableAgent: RunnableSequence;
+  private modelWithStop;
+  private executor: AgentExecutor;
+  constructor(private configService: ConfigService) {}
   public setPhoneNumber(phoneNumber: string): void {
     this.phoneNumber = phoneNumber;
   }
@@ -53,7 +50,7 @@ export class CharlesBot implements BotInterface {
     if (!userBuffer) {
       this.memory = new BufferMemory({
         returnMessages: true,
-        memoryKey: 'history',
+        memoryKey: 'chat_history',
       });
     } else {
       this.memory = userBuffer;
@@ -61,8 +58,46 @@ export class CharlesBot implements BotInterface {
   }
   async getAnswer(user: string, input: string): Promise<string> {
     this.setMemory(user);
-    const response = await this.chain.call({
-      text: input,
+
+    const model = new OpenAI({
+      modelName: 'gpt-3.5-turbo',
+      openAIApiKey: this.configService.get<string>('OPENAI_API_KEY'),
+    });
+    const modelWithStop = model.bind({
+      stop: ['\nObservation'],
+    });
+    const tools = [
+      new DynamicTool({
+        name: 'saveEmail',
+        description:
+          'call this to save the email of the user, input shoud be a string ',
+        func: async (input: string) => {
+          console.log(input);
+          return input;
+        },
+      }),
+    ];
+    // this.memory = new BufferMemory({
+    //   returnMessages: true,
+    //   memoryKey: 'chat_history',
+    // });
+    const prompt = PromptTemplate.fromTemplate(charlesPromptTemplate);
+
+    const toolNames = tools.map((tool) => tool.name);
+
+    // const promptWithInputs = prompt.partial({
+    //   tools: renderTextDescription(this.tools),
+    //   tool_names: toolNames.join(', '),
+    // });
+
+    const executor = await initializeAgentExecutorWithOptions(tools, model, {
+      agentType: 'chat-conversational-react-description',
+      verbose: true,
+      memory: this.memory,
+    });
+
+    const response = await executor.call({
+      input,
     });
     this.memory.saveContext(
       {
@@ -71,7 +106,7 @@ export class CharlesBot implements BotInterface {
       { output: response?.response },
     );
     this.userHistory.set(user, this.memory);
-    return response?.response;
+    return response?.output;
   }
 }
 
--- src/bots/prompts/asistant-prompt.ts
+++ src/bots/prompts/asistant-prompt.ts
@@ -19,4 +19,74 @@ Los productos de la empresa son:
 -Integraciones dedicadas.
 Si te preguntan sobre otro tema no relacionado con botPulse, debes responder amablemente que esa no es tu funcion.
 Si te preguntan precios de los sercios, debes ofrecer agendar una llamada.
-Respone amablemente y no excedas mas de 30 palabras: {text}`;
+Respone amablemente y no excedas mas de 30 palabras
+
+HERRAMIENTAS:
+------------
+
+Tienes acceso a las siguientes herramientas:
+{tools}
+
+Para usar una herramienta, utiliza el siguiente formato:
+
+'''
+Thought: ¿Necesito usar una herramienta? Si
+Action: La acion a tomar, debe ser una de [{tool_names}]
+Action Input: El input de la accion
+Observation: El resultado de la accion
+'''
+Cuando tengas una respuesta para decir al humano, o si no necesitas usar una herramienta, debes usar el formato:
+
+'''
+Thought: No necesito usar una herramienta
+Final Answer: [tu respuesta aqui]
+'''
+
+¡Inicia!
+
+Previous conversation history:
+{chat_history}
+
+New input: {input}
+{agent_scratchpad}
+`;
+
+// const testAgentPrompt = `
+// Assistant is a large language model trained by OpenAI.
+
+// Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+// Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+// Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+// TOOLS:
+// ------
+
+// Assistant has access to the following tools:
+
+// {tools}
+
+// To use a tool, please use the following format:
+
+// '''
+// Thought: Do I need to use a tool? Yes
+// Action: the action to take, should be one of [{tool_names}]
+// Action Input: the input to the action
+// Observation: the result of the action
+// '''
+// When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+
+// '''
+// Thought: Do I need to use a tool? No
+// Final Answer: [your response here]
+// '''
+
+// Begin!
+
+// Previous conversation history:
+// {chat_history}
+
+// New input: {input}
+// {agent_scratchpad}
+// `;

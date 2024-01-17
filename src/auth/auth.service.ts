a40738aa807617e32e3bfdef290fecced0ff5f8a
Gerson <g.mayerduran@gmail.com>
2024-01-17 15:13:13 -0500

    Feat: solved issue with username-email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -23,7 +23,7 @@ export class AuthService {
   }
 
   async login(user: { email: string; _id: string }): Promise<any> {
-    const payload = { username: user.email, sub: user._id };
+    const payload = { email: user.email, sub: user._id };
     return {
       access_token: this.jwtService.sign(payload),
     };

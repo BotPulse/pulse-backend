b386d5b54dffc64890d5728fb48f655d3e0e87e5
Gerson <g.mayerduran@gmail.com>
2024-01-24 15:48:42 -0500

    Feat: added data validation for CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
+import { IsNotEmpty, IsEmail, Length, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail()
   email: string;
+  @Length(6, 50, { message: 'Password length must be between 6 and 50' })
   password: string;
+  @IsNotEmpty({ message: 'Please enter a valid first name' })
+  @IsString({ message: 'Please enter a valid first name' })
   firstName: string;
+  @IsNotEmpty({ message: 'Please enter a valid last name' })
+  @IsString({ message: 'Please enter a valid last name' })
   lastName: string;
 }
